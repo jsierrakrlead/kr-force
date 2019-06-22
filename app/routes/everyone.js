cdf54ff8bffe9ec93199f8ded1eaf63deb9c2ea6
jsierra <jsierra@kaufmanrossin.com>
2019-06-22 19:40:42 -0400

    graqhpl import file

--- app/routes/everyone.js
+++ app/routes/everyone.js
@@ -1,5 +1,11 @@
 import Route from '@ember/routing/route';
+import RSVP from 'rsvp';
 import { RouteQueryManager } from "ember-apollo-client";
+import query from "../gql/queries/user-skill-levels.graphql";
 
 export default Route.extend(RouteQueryManager, {
+  model(){
+    console.log('dfs;ksdaf', query)
+    return this.get('apollo').watchQuery({ query });
+  }
 });
