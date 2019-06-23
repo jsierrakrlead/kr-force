20be10d8822109c9e60ba9c5aa6efde325bfe025
jsierra <jsierra@kaufmanrossin.com>
2019-06-22 20:44:47 -0400

    some work on the routing

--- app/routes/everyone.js
+++ app/routes/everyone.js
@@ -5,8 +5,12 @@ import { hash } from 'rsvp';
 export default Route.extend(RouteQueryManager, {
   model(){
     return hash({
-      users: this.get('apollo').watchQuery({ query: users }),
-      skills: this.get('apollo').watchQuery({ query: skills })
+      users: this.get('apollo').watchQuery({ query: users }).then((query)=>{
+        return query.users;
+      }),
+      skills: this.get('apollo').watchQuery({ query: skills }).then((query)=>{
+        return query.skills;
+      })
     })
   }
 });
