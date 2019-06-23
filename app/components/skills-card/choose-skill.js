2caefcaf467299ca85587f25269cbbb71e3a96e4
jsierra <jsierra@kaufmanrossin.com>
2019-06-23 18:36:35 -0400

    search bar type ahead trying to work

--- app/components/skills-card/choose-skill.js
+++ app/components/skills-card/choose-skill.js
@@ -2,5 +2,9 @@ import Component from '@ember/component';
 import EmberObject, { computed } from '@ember/object';
 export default Component.extend({
   chosenLevel:null,
-
+  actions:{
+    searchSkills(val){
+      console.log('serarching', val)
+    }
+  }
 });
