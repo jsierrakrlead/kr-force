b2115bcc973aa083e825008686965837cd80d33c
jsierra <jsierra@kaufmanrossin.com>
2021-04-08 16:51:00 -0400

    adding automatic transition

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,11 +1,14 @@
-import Route from '@ember/routing/route';
+import Route from "@ember/routing/route";
 import { RouteQueryManager } from "ember-apollo-client";
-import { skills } from './application/query';
-import { inject as service } from '@ember/service';
+import { skills } from "./application/query";
+import { inject as service } from "@ember/service";
 
-
-export default Route.extend(RouteQueryManager,{
-  model(){
-    return this.get('apollo').watchQuery({ query: skills }, "skills");
-  }
-})
+export default Route.extend(RouteQueryManager, {
+  model() {
+    console.log("hello");
+    return this.get("apollo").watchQuery({ query: skills }, "skills");
+  },
+  afterModel() {
+    this.transitionTo("everyone");
+  },
+});
