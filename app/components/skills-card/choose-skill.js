decfad4cf46609f49c6a50d8070ff83c006e07c9
jsierra <jsierra@kaufmanrossin.com>
2021-04-08 16:19:31 -0400

    krforce skill set

--- app/components/skills-card/choose-skill.js
+++ app/components/skills-card/choose-skill.js
@@ -1,47 +1,52 @@
-import Component from '@ember/component';
+import Component from "@ember/component";
 import { ComponentQueryManager } from "ember-apollo-client";
-import { run } from '@ember/runloop';
+import { run } from "@ember/runloop";
 import { searchSkillsByName } from "../../routes/my-skills/query";
-import { A } from '@ember/array';
-import { computed , observer } from '@ember/object';
-import { setProperties } from '@ember/object';
+import { A } from "@ember/array";
+import { computed, observer } from "@ember/object";
+import { setProperties } from "@ember/object";
 
 export default Component.extend(ComponentQueryManager, {
-  chosenLevel:null,
+  chosenLevel: null,
   chosenSkill: null,
 
-  disabled: computed('chosenLevel', 'chosenSkill', function(){
-    return !this.get('chosenSkill') || !this.get('chosenLevel');
+  disabled: computed("chosenLevel", "chosenSkill", function () {
+    return !this.get("chosenSkill") || !this.get("chosenLevel");
   }),
 
-  setQuery: observer('chosenSkill', function(){
-    this.get('chosenSkill') && this.set('query', this.get('chosenSkill.name'));
+  setQuery: observer("chosenSkill", function () {
+    this.get("chosenSkill") && this.set("query", this.get("chosenSkill.name"));
   }),
 
-  search(){
+  search() {
     let variables = {
-      name: this.get('query')
-    }
-    this.get('apollo').watchQuery( { query: searchSkillsByName, variables }, "searchSkills").then((data)=>{
-      this.set('chosenSkill', null);
-      return this.set('queryResults', A(data));
-    })
+      name: this.get("query"),
+    };
+    this.get("apollo")
+      .watchQuery({ query: searchSkillsByName, variables }, "searchSkills")
+      .then((data) => {
+        this.set("chosenSkill", null);
+        return this.set("queryResults", A(data));
+      });
   },
-  actions:{
-    searchSkills(){
-      if (this.get('query')) {
-        run(this, 'search')
+  actions: {
+    searchSkills() {
+      if (this.get("query")) {
+        run(this, "search");
       } else {
-        this.set('chosenSkill', null);
-        this.set('queryResults', A());
+        this.set("chosenSkill", null);
+        this.set("queryResults", A());
       }
     },
-    clearSearch(){
+    addSkill() {
+      alert("Skills should not be added yet");
+    },
+    clearSearch() {
       setProperties(this, {
         chosenLevel: null,
         query: null,
-        chosenLevel: null
+        chosenLevel: null,
       });
-    }
-  }
+    },
+  },
 });
